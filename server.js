4fc9601c9fb102a317177fe9965eca95bd7ba564
Amol <amol.chawathe@fountaintechies.com>
2014-10-18 18:28:16 +0800

    First Commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var apn = require('apn');
+ 
+var token = "Your device token";
+var device = new apn.Device(token);
+ 
+var notification = new apn.Notification();
+notification.expiry = Math.floor(Date.now() / 1000) + 3600;
+notification.badge = 1;
+notification.alert = "This is a Push Notification=)";
+notification.payload = {'prop': 'special value'};
+notification.device = device;
+ 
+var options = {
+ gateway: 'gateway.sandbox.push.apple.com',
+ cert: 'assets/housingadminCer.pem',
+ key: 'assets/housingadminKey.pem',
+ passphrase: ‘housingadmin’
+}
+var apnsConnection = new apn.Connection(options);
+apnsConnection.pushNotification(notification, device);
